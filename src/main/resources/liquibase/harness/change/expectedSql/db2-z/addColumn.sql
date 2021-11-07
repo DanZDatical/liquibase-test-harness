93ec6b634de9f475775c23c748b584c02fa7a5c0
AlexanderSashchenko <santys87@gmail.com>
2021-11-07 19:38:36 +0200

    db2z added fixed change types

--- src/main/resources/liquibase/harness/change/expectedSql/db2-z/addColumn.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/db2-z/addColumn.sql
@@ -1,6 +1,7 @@
-ALTER TABLE "LTHUSER".authors ADD varcharColumn VARCHAR(25)
-ALTER TABLE "LTHUSER".authors ADD intColumn INTEGER
-ALTER TABLE "LTHUSER".authors ADD dateColumn date
-UPDATE "LTHUSER".authors SET varcharColumn = 'INITIAL_VALUE'
-UPDATE "LTHUSER".authors SET intColumn = 5
-UPDATE "LTHUSER".authors SET dateColumn = DATE('2020-09-21')
+CREATE TABLE "LTHUSER".add_column_test (id INTEGER NOT NULL, CONSTRAINT PK_ADD_COLUMN_TEST PRIMARY KEY (id))
+ALTER TABLE "LTHUSER".add_column_test ADD varcharColumn VARCHAR(25)
+ALTER TABLE "LTHUSER".add_column_test ADD intColumn INTEGER
+ALTER TABLE "LTHUSER".add_column_test ADD dateColumn date
+UPDATE "LTHUSER".add_column_test SET varcharColumn = 'INITIAL_VALUE'
+UPDATE "LTHUSER".add_column_test SET intColumn = 5
+UPDATE "LTHUSER".add_column_test SET dateColumn = DATE('2020-09-21')
