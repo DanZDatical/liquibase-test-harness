d6d07c0f69dfb19569fa5e84824df450e186f578
KushnirykOleh <kushnirykoleh@gmail.com>
2021-12-08 18:26:10 +0200

    updated expected query

--- src/main/resources/liquibase/harness/change/expectedSql/postgresql/addNotNullConstraintWithValues.sql
+++ src/main/resources/liquibase/harness/change/expectedSql/postgresql/addNotNullConstraintWithValues.sql
@@ -1,5 +1,4 @@
 CREATE TABLE public.lb45 (name CHAR(20))
 INSERT INTO public.lb45 (name) VALUES ('marcel')
-ALTER TABLE public.lb45 ADD "columnWithInitialValue" BIGINT
+ALTER TABLE public.lb45 ADD "columnWithInitialValue" BIGINT NOT NULL
 UPDATE public.lb45 SET "columnWithInitialValue" = 0
-ALTER TABLE public.lb45 ALTER COLUMN  "columnWithInitialValue" SET NOT NULL
